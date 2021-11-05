518def185cfe6fec2b2a40266c9a25081d6e01e4
Marcius Bezerra <marciusbezerra@outlook.com>
2021-11-05 03:39:25 -0300

    código para a camera e geo-location

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  location: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+    const video = document.getElementById('video') as any;
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+        video.srcObject = stream;
+        video.play();
+      });
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(data => {
+        this.location = `${data.coords.latitude},${data.coords.longitude}`;
+      }, err => {
+        this.location = err;
+      }, { enableHighAccuracy: true });
+    }
+  }
+
+  ionViewDidLoad() {
+
+  }
 
 }
